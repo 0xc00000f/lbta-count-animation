45a8515fc4ec7dc18736724a373bfbfb99bb9b15
Maxim Tsyganov <0xc00000f@gmail.com>
2022-04-27 01:07:06 +0300

    lesson ended

--- lbta-count-animation/ViewController.swift
+++ lbta-count-animation/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let countingLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.text = "1234"
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupLayout()
+
+        setupAnimation()
     }
 
+    private func setupLayout() {
+        view.backgroundColor = .red
+        [countingLabel].forEach { view.addSubview($0) }
+
+        NSLayoutConstraint.activate([
+            countingLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            countingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            countingLabel.widthAnchor.constraint(equalTo: view.widthAnchor),
+            countingLabel.heightAnchor.constraint(equalTo: countingLabel.widthAnchor)
+        ])
+
+    }
+
+    private func setupAnimation() {
+        let displayLink = CADisplayLink(target: self, selector: #selector(handleUpdate))
+        displayLink.add(to: .main, forMode: .default)
+    }
+
+    var startValue: Double = 0
+    let endValue: Double = 12000
+
+    let animationStartDate = Date()
+    let animationDuration = 1.5
+
+    @objc func handleUpdate() {
+
+        let now = Date()
+        let elapsedTime = now.timeIntervalSince(animationStartDate)
+
+        if elapsedTime > animationDuration {
+            self.countingLabel.text = "\(endValue)"
+        } else {
+            let percentage = elapsedTime / animationDuration
+            let value = startValue + percentage * (endValue - startValue)
+            self.countingLabel.text = "\(value)"
+        }
+
+    }
 
 }
 
